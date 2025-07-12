f5822cdbbe46d7b9740276cf8e4144f24888719e
Arushi-15 <thearushiraj1508@gmail.com>
2025-07-12 11:38:34 +0530

    Update Task3.py

--- Week 4/Task3.py
+++ Week 4/Task3.py
@@ -1,1 +1,12 @@
+/*Weather Observation Station 19 : Query the Euclidean Distance between points and and format your answer to display decimal digits.
+*/
+  SELECT 
+  CAST(
+    SQRT(
+      POWER(MAX(LAT_N) - MIN(LAT_N), 2) + 
+      POWER(MAX(LONG_W) - MIN(LONG_W), 2)
+    ) 
+  AS DECIMAL(10, 4)) 
+AS DISTANCE
+FROM STATION;
 
