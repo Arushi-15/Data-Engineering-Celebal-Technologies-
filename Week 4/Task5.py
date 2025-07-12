9c081867407ab38993d2292adca44f5ef91ce1db
Arushi-15 <thearushiraj1508@gmail.com>
2025-07-12 12:04:44 +0530

    Update Task5.py

--- Week 4/Task5.py
+++ Week 4/Task5.py
@@ -1,1 +1,6 @@
+/*African Cities : Query the names of all cities where the CONTINENT is 'Africa'.*/
+SELECT ci.NAME
+FROM CITY ci, COUNTRY co
+WHERE ci.COUNTRYCODE = co.CODE
+  AND co.CONTINENT = 'Africa';
 
