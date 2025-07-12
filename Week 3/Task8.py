786ac8385d9fcac08460e4925387966b96d480c3
Arushi-15 <thearushiraj1508@gmail.com>
2025-07-12 10:11:31 +0530

    Update Task8.py

--- Week 3/Task8.py
+++ Week 3/Task8.py
@@ -1,1 +1,16 @@
+/*Weather Observation Station 5 : Query the two cities in STATION with the shortest and longest CITY names, as well as their respective lengths*/
+
+  (
+  SELECT CITY, LENGTH(CITY) AS NAME_LENGTH
+  FROM STATION
+  ORDER BY LENGTH(CITY) ASC, CITY ASC
+  LIMIT 1
+)
+UNION
+(
+  SELECT CITY, LENGTH(CITY) AS NAME_LENGTH
+  FROM STATION
+  ORDER BY LENGTH(CITY) DESC, CITY ASC
+  LIMIT 1
+);
 
